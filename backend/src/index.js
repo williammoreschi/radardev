1fc8917a99e188bfdd8017105188962e6c867ce1
William Moreschi <william.moreschi@guiadomarceneiro.net>
2020-01-17 01:20:43 -0300

    Deixar o node ser enxergado por outras aplicação; Correção avatar_url estava escrito errado;

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const routes = require("./routes");
 
 const app = express();
-
-mongoose.connect("mongodb+srv://<user>:<password>@cluster0-ppxph.mongodb.net/week10?retryWrites=true&w=majority",{
+let user = "<user>";
+let pass = "<pass>";
+mongoose.connect(`mongodb+srv://${user}:${pass}@cluster0-ppxph.mongodb.net/week10?retryWrites=true&w=majority`,{
     useNewUrlParser: true,
     useUnifiedTopology: true
 
 })
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.listen(3333);
